171ee63c8e7f931bed7e622b47c7a5a1b01625b8
dariomlopez <marotodario@gmail.com>
2023-06-12 16:56:14 +0200

    node como html

--- Node/primerNode.js
+++ Node/primerNode.js
@@ -1,11 +1,11 @@
-var http = require("http");
+var http = require("http");  //require module "http"
 
 http.createServer(function(request, answer){
-    answer.writeHead(200, {"Content-Type": "text/plain"});
-    answer.end(`
+    answer.writeHead(200, {"Content-Type": "text/html, charset=UTF-8"});
+    answer.end(`<h1>
     Hola
     desde
     mi primer
     node
-    js`);
+    js</h1>`);   //response to the client
 }).listen(8080);
