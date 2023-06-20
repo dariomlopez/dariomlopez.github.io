90114743ab1e6c0ace12e8e5909e632801dab9e0
dariomlopez <marotodario@gmail.com>
2023-06-20 16:50:59 +0200

    examen

--- Examen 21_06_2023/server.js
+++ Examen 21_06_2023/server.js
@@ -1,10 +1,10 @@
+/** Importar los módulos necesarios
+ */
 const mysql = require("mysql");
 const http = require("http");
 
-// Importar los módulos necesarios
-
 // Crear una conexión a la base de datos MySQL
-const con = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
@@ -12,54 +12,55 @@ const con = mysql.createConnection({
 });
 
 // Consultas SQL
+/** En este query seleccionamos todas las columnas de la tabla tblUsuarios con limite de 10 filas */
 const selectAll = "SELECT * FROM tblUsuarios LIMIT 10;";
+/**En este query seleccionamos todas las columnas de la tabla tblUsuarios para un cliente o usuario que coincida con el id deseado */
 const selectOne = "SELECT * FROM tblUsuarios WHERE idx = ?;";
-const postQuery =
-  "INSERT INTO tblUsuarios (idx, usuario, nombre, sexo, nivel, email, telefono, marca, compañia, saldo, activo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+/** Creamos un usuario nuevo con las columnas indicadas */
+const postQuery = "INSERT INTO tblUsuarios (idx, usuario, nombre, sexo, nivel, email, telefono, marca, compañia, saldo, activo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
 
-// Establecer la conexión con la base de datos
-con.connect(function(err) {
+// Establecemos la conexión con la base de datos
+connection.connect(function(err) {
   if (err) throw err;
   console.log("Conexión exitosa a la base de datos 'examen'");
 
-  // Crear un servidor HTTP
-  http
-    .createServer(function(req, res) {
-      // Obtener el ID del usuario de la URL
-      let id = req.url.split("/")[2];
+  // Creamos un servidor HTTP con parametros request y response
+  http.createServer(function(request, response) {
+      // Obtenemos el ID del usuario a traves de la URL con el método split que devuelve un array de un string y elegimos el segundo valor del array que es el ID
+      let id = request.url.split("/")[2];
 
-      // Manejar la solicitud GET para obtener todos los usuarios de la tabla
-      if (req.url.startsWith("/usuarios") && !id && req.method === "GET") {
-        // Ejecutar la consulta SELECT para obtener todos los usuarios
-        con.query(selectAll, function(err, result) {
+      // Con un if controlamos si la url no tiene un id y es de metodo get. Si true ejecutamos el query guardado en la variable selectAll
+      if (request.url.startsWith("/usuarios") && !id && request.method === "GET") {
+        // Ejecutar el query selectAll para obtener todos los usuarios
+        connection.query(selectAll, function(err, result) {
           if (err) throw err;
           console.log("Lista de datos");
 
           // Configurar la respuesta con el tipo de contenido y enviar los datos en formato JSON
-          res.writeHead(200, { "Content-type": "application/json; charset=UTF-8;" });
-          res.end(JSON.stringify(result, null, 2));
+          response.writeHead(200, { "Content-type": "application/json; charset=UTF-8;" });
+          response.end(JSON.stringify(result, null, 2));
         });
       }
-      // Manejar la solicitud GET para obtener un usuario específico por su ID
-      else if (req.url.startsWith("/usuarios/") && id && req.method === "GET") {
+      /* Manejar la solicitud GET para obtener un usuario específico por su ID. Si la url contiene "/usuarios/" */
+      else if (request.url.startsWith("/usuarios/") && id && request.method === "GET") {
         // Ejecutar la consulta SELECT para obtener el usuario con el ID especificado
-        con.query(selectOne, [id], function(err, result) {
+        connection.query(selectOne, [id], function(err, result) {
           if (err) throw err;
           console.log("Un dato");
 
           // Configurar la respuesta con el tipo de contenido y enviar el usuario encontrado en formato JSON
-          res.writeHead(200, { "Content-type": "application/json; charset=UTF-8;" });
-          res.end(JSON.stringify(result, null, 2));
+          response.writeHead(200, { "Content-type": "application/json; charset=UTF-8;" });
+          response.end(JSON.stringify(result, null, 2));
         });
       }
       // Manejar la solicitud POST para crear un nuevo usuario
-      else if (req.url.startsWith("/usuarios/") && id && req.method === "POST") {
+      else if (request.url.startsWith("/usuarios/") && id && request.method === "POST") {
         let jsonString = "";
-        req.on("data", function(data) {
+        request.on("data", function(data) {
           // Leer los datos enviados por el cliente y almacenarlos en una variable
-          jsonString += data;
+          jsonString = jsonString + data;
         });
-        req.on("end", function() {
+        request.on("end", function() {
           // Convertir el JSON recibido a un objeto JavaScript
           const jsonData = JSON.parse(jsonString);
 
@@ -69,11 +70,11 @@ con.connect(function(err) {
           // Verificar si se proporcionaron todos los datos necesarios para crear un nuevo usuario
           if (!idx || !usuario || !nombre || !sexo || !nivel || !email || !telefono || !marca || !compañia || !saldo || !activo) {
             // Si falta algún dato, enviar una respuesta de "Bad Request" al cliente
-            res.writeHead(400, { "Content-type": "text/plain" });
-            res.end("Bad Request: Faltan campos obligatorios.");
+            response.writeHead(400, { "Content-type": "text/plain" });
+            response.end("Bad Request: Faltan campos obligatorios.");
           } else {
             // Ejecutar la consulta INSERT para insertar el nuevo usuario en la base de datos
-            con.query(
+            connection.query(
               postQuery,
               [idx, usuario, nombre, sexo, nivel, email, telefono, marca, compañia, saldo, activo],
               function(err, result) {
@@ -81,8 +82,8 @@ con.connect(function(err) {
                 console.log(`Se ha creado ${result.affectedRows} dato con id ${result.insertId}`);
 
                 // Configurar la respuesta con el código de estado 201 (Created)
-                res.writeHead(201, { "Content-type": "application/json; charset=UTF-8;" });
-                res.end();
+                response.writeHead(201, { "Content-type": "application/json; charset=UTF-8;" });
+                response.end();
               }
             );
           }
@@ -91,8 +92,8 @@ con.connect(function(err) {
       // Manejar cualquier otra solicitud que no coincida con las anteriores
       else {
         // Enviar una respuesta de "Not Found" al cliente
-        res.writeHead(404, { "Content-type": "text/plain" });
-        res.end("Not Found");
+        response.writeHead(404, { "Content-type": "text/plain" });
+        response.end("Not Found");
       }
     })
     .listen(8080, () => {
