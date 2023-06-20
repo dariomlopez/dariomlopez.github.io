4b6ed0101f6001647f00bfe7d5676fd18e984aa6
dariomlopez <marotodario@gmail.com>
2023-06-20 15:40:00 +0200

    examen

--- Node/conectionNodeMySQL.js
+++ Node/conectionNodeMySQL.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "tabla_prueba"
+  database: "tabla_prueba"  /**Cambiar base de datos */
 });
 
 connection.connect(function(err) {
