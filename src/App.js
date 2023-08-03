d06510395dfb9a3a2f7e6564fb857d557a7cb564
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-08-03 19:18:12 +0200

    trying to center

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="flex w-screen h-screen items-center justify-center">
-      <h1 className="text-5xl text-blue-500 font-black">Dario Maroto Portfolio</h1>
-      <h1 className="text-5xl text-blue-500 font-black">- Under construction -</h1>
+    <div className="flex flex-col w-screen h-screen items-center justify-center">
+      <h1 className="text-5xl text-blue-500 font-black text-center">Dario Maroto Portfolio</h1>
+      <h1 className="text-5xl text-blue-500 font-black text-center">- Under construction -</h1>
       <br/>
       <p>lalalalal</p>
     </div>
