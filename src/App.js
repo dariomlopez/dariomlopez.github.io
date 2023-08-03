ef92068a7c3cb6f4063d54cfcee86a50a79b24d0
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-08-03 18:41:58 +0200

    changed h1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="flex w-screen h-screen items-center justify-center">
-      <h1 className="text-5xl text-blue-500 font-black	">Under construction</h1>
+      <h1 className="text-5xl text-blue-500 font-black">Dario Maroto Portfolio - Under construction</h1>
+      <br/>
       <p>lalalalal</p>
     </div>
   );
