8e03988ed7a6a9898413f8bf65950780c2892fd1
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-10-17 15:15:13 +0200

    Actualizando mi portfolio para cambio de idioma y añadir modo oscuro

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 
-// Cómo cambiar de idioma una página web --> https://www.youtube.com/watch?v=hBYVxQLtrqQ&ab_channel=DorianDesings
+// TODO 
+
+// [] Cómo cambiar de idioma una página web https://www.youtube.com/watch?v=hBYVxQLtrqQ&ab_channel=DorianDesings */
+
+// [] dark mode 
+// https://bootcamp.uxdesign.cc/create-a-dark-mode-for-your-website-d62e73a4275d
+
+// https://www.geeksforgeeks.org/how-to-create-dark-light-mode-for-website-using-javascript-jquery/
+
+// https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/#aa-using-custom-properties
 
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
