55b72b69e1a285eb13c1a021612bcc13c10c8e54
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-10-18 10:48:09 +0200

    Añadiendo comentarios JS

--- script.js
+++ script.js
@@ -3,32 +3,30 @@
 
 // [] Cómo cambiar de idioma una página web https://www.youtube.com/watch?v=hBYVxQLtrqQ&ab_channel=DorianDesings */
 
-// [] dark mode 
+// [x] dark mode 
 // https://bootcamp.uxdesign.cc/create-a-dark-mode-for-your-website-d62e73a4275d
 
 // https://www.geeksforgeeks.org/how-to-create-dark-light-mode-for-website-using-javascript-jquery/
 
 // https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/#aa-using-custom-properties
 
+/** Elementos del DOM para cambio de tema*/
 const page = document.querySelector(".page");
 const toggle = document.querySelector(".toggle-input");
 const toggleIcon = page.querySelector(".toggle-icon");
 
+/** Elementos del DOM para cambio de idioma */
+
+/** Función para cambiar el menu del navbar */
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+/**------------------------------------ */
 
-// function bodyReplaceClass() {
-//   if(toggle.checked) {
-//     page.classList.replace("light", "dark")
-//   } else {
-//     page.classList.replace("dark", "light");
-//   }
-// }
-
+/** Función para cambiar el icono cuando cambia el modo */
 function toggleIconTheme() {
   if(page.classList.contains("dark")) {
     toggleIcon.src = "./assets/light.png";
@@ -39,22 +37,29 @@ function toggleIconTheme() {
   }
 }
 
+// Verifica si hay un estado previamente guardado en el almacenamiento local
 function checkedState() {
   if (!(localStorage.checked === undefined)) {
+    // Establece el estado del checkbox al valor guardado en el almacenamiento local
     toggle.checked = isTrue(localStorage.getItem("checked"));
+    // Aplica el tema de acuerdo al estado actual del checkbox
     toggleTheme();
   }
 }
 
+/** Cambia la clase del body añadiendo o eliminando la clase */
 function toggleTheme() {
   if(toggle.checked) {
+    /** Si el checkbox está marcado aplica el tema oscuro */
     page.classList.remove("light");
     page.classList.add("dark")
   } else {
+    /** Si el checkbox esta desmarcado aplic el tema claro */
     page.classList.remove("dark");
     page.classList.add("light")
   }
-    // bodyReplaceClass();
+
+  /** Llamamos a la función para que cambie el icono */
   toggleIconTheme();
   localStorage.setItem("checked", toggle.checked);
 }
@@ -63,6 +68,8 @@ function isTrue(value) {
     return value === "true";
 }
 
+/** Verificar el estado inicial del checkbox al cargar la página */
 checkedState();
 
+/** Escucha cambios en el checkbox y aplica el tema correspondiente */
 toggle.addEventListener("change", toggleTheme);
