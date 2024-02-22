dd7bb0bdcd8ed657873dbccf5c01e964a09da1ac
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2024-02-22 16:29:55 +0100

    portfolio

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 
-// TODO 
-
-// [] Cómo cambiar de idioma una página web https://www.youtube.com/watch?v=hBYVxQLtrqQ&ab_channel=DorianDesings */
-
-// [x] dark mode 
-// https://bootcamp.uxdesign.cc/create-a-dark-mode-for-your-website-d62e73a4275d
-
-// https://www.geeksforgeeks.org/how-to-create-dark-light-mode-for-website-using-javascript-jquery/
-
-// https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/#aa-using-custom-properties
-
 /** Elementos del DOM para cambio de tema*/
 
 const page = document.querySelector(".page");
